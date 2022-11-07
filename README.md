9c02fba578487dc1db2f97a8845aeacb5b0dc9cf
moanrisy <moanrisy@gmail.com>
2022-11-07 15:52:17 +0700

    First version

---
+++ README.md
@@ -0,0 +1,25 @@
+# Clock in Task Fyne
+
+Simple program to show current task that I'm working on
+
+# How it work
+
+## Fyne toolkit
+
+Using fyne toolkit to create desktop app using go language. The app will show current task that I currently clocked-in using emacs
+
+## Emacs function
+
+`;; Hook for clock-in
+(defun write-clock-in-title-hook()
+"Write clock in title into a file"
+(message (symbol-value 'org-clock-heading))
+(append-to-file (symbol-value 'org-clock-heading) nil "~/clock-in-title")
+(append-to-file "\n" nil "~/clock-in-title")
+)
+
+(add-hook 'org-clock-in-hook 'write-clock-in-title-hook)`
+
+## Power toys
+
+This 3rd party app used to make window in window 11 can be used as 'always on top'
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Clock in Task")
+	dirname, _ := os.UserHomeDir()
+	filePath := dirname + "/clock-in-title"
+	file, err := os.Open(filePath)
+	checkErr(err)
+
+	defer func() {
+		if err = file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	cit := widget.NewLabel("Please clock in your task")
+	w.SetContent(cit)
+
+	initialStat, err := os.Stat(filePath)
+	checkErr(err)
+	go func() {
+		for {
+			stat, err := os.Stat(filePath)
+			checkErr(err)
+			if stat.Size() != initialStat.Size() || stat.ModTime() != initialStat.ModTime() {
+
+				buf := make([]byte, stat.Size())
+				_, err := file.Read(buf)
+				fmt.Println(string(buf))
+				cit.SetText(string(buf))
+
+				initialStat, err = os.Stat(filePath)
+				checkErr(err)
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	w.Resize(fyne.NewSize(200, 50))
+	w.ShowAndRun()
+}
